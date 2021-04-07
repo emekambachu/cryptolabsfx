78bd96e028de4571075de1da5c7e9e7d6966d1e9
Emeka Mbachu <victor.c.mbachu@gmail.com>
2021-04-07 13:30:19 +0100

    added tawk

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -161,3 +161,16 @@
 <script src="{{ asset('main/js/map.js') }}"></script>
 <script src="{{ asset('main/js/main.js') }}"></script>
 
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/606da4a3f7ce18270937f077/1f2m4m888';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
