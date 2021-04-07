ce464eac9840df5ff5a0ce8af2432d92d8dac771
Emeka Mbachu <victor.c.mbachu@gmail.com>
2021-04-07 17:09:11 +0100

    completed admin login

--- resources/views/auth/admin-login.blade.php
+++ resources/views/auth/admin-login.blade.php
@@ -4,78 +4,76 @@
     Admin Login
 @endsection
 
-@section('contents')
-    <div class="banner-area" id="banner-area" style="background-image:url({{ asset('header.jpg') }});">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col">
-                    <div class="banner-heading">
-                        <h2 class="banner-title">Admin Login</h2>
-                        <ol class="breadcrumb">
-                            <li><a href="{{ url('/') }}">Home</a></li>
-                            <li>Admin Login</li>
-                        </ol>
-                    </div>
-                </div>
-                <!-- Col end-->
-            </div>
-            <!-- Row end-->
-        </div>
-        <!-- Container end-->
+@section('content')
+    <div class="page-title" style="background-image:url({{ asset('main/img/bg/bg5.jpg') }})">
+        <h1>Admin Login</h1>
+        <ul>
+            <li><a href="{{ url('/') }}">Home</a></li>
+            <li><a href="">Admin Login</a></li>
+        </ul>
     </div>
 
-    <section class="main-container" id="main-container" style="background-color: #fffefe; margin-bottom: 100px;">
-        <!-- container end-->
-        <div class="gap-60"></div>
-        <div class="ts-form" id="ts-form">
-            <div class="container">
+    <div class="section-block">
+        <div class="container">
+            <div class="center-holder section-heading">
+                <h2>Admin Login</h2>
+                <div class="section-heading-line"></div>
+            </div>
 
+            <div class="mt-40">
                 @include('includes.alerts')
 
-                <div class="row">
-                    <div class="col-lg-7 col-12">
+                <form method="POST" action="{{ route('admin-login') }}" enctype="multipart/form-data">
+                    @csrf
+                    <div class="row">
+                        <div class="col-md-6">
+                            <!-- Input Field Starts -->
+                            <div class="form-group">
+                                <label>Username</label>
+                                <input class="form-control @error('username') is-invalid @enderror" type="text" name="username"
+                                       value="{{ old('username') }}" placeholder="Username *" required>
+                                @error('username')
+                                <span class="invalid-feedback text-warning" role="alert">
+                            <strong>{{ $message }}</strong></span>
+                                @enderror
+                            </div>
+                            <!-- Input Field Ends -->
+                        </div>
 
-                        <form class="contact-form" method="POST" action="{{ route('admin-login') }}">
-                            @csrf
-                            <div class="row">
-
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Username</label>
-                                        <input class="form-control @error('username') is-invalid @enderror" type="text" name="username"
-                                               value="{{ old('username') }}" placeholder="Username *" required>
-                                        @error('username')
-                                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                        @enderror
-                                    </div><!-- /.form-grp -->
-                                </div><!-- /.col-md-6 -->
-
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Password</label>
-                                        <input class="form-control @error('password') is-invalid @enderror" type="password" name="password"
-                                               placeholder="password" required>
-                                        @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                        @enderror
-                                    </div><!-- /.form-grp -->
-                                </div><!-- /.col-md-6 -->
-
-                                <div class="col-md-12" style="margin-top: 6px;">
-                                    <button class="btn btn-primary tw-mt-30" type="submit">Login</button>
-                                </div><!-- /.col-md-6 -->
-
-                            </div><!-- /.row -->
-                        </form>
-
+                        <div class="col-md-6">
+                            <!-- Input Field Starts -->
+                            <div class="form-group">
+                                <label>Password</label>
+                                <input class="form-control @error('password') is-invalid @enderror"
+                                       type="password" name="password"
+                                       placeholder="password" required>
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong></span>
+                                @enderror
+                            </div>
+                            <!-- Input Field Ends -->
+                        </div>
                     </div>
 
-                </div>
+                    <div class="form-group">
+                        <div style="display: block;">
+                            <label>Remember me</label>
+                            <input style="display: inline; width: 50px;" type="checkbox" name="remember" id="remember"
+                                {{ old('remember') ? 'checked' : '' }}>
+                        </div>
+                    </div><!-- /.form-grp -->
+
+                    <div class="col-md-12">
+                        <div class="center-holder">
+                            <button style="background-color: #EEB117; padding: 5px;"
+                                    type="submit">Login</button>
+                        </div>
+                    </div>
+                </form>
+
             </div>
+
         </div>
-    </section>
+    </div>
 @endsection
