2e0055d14935552f7b09066cd6fa2306ea532291
Emeka Mbachu <victor.c.mbachu@gmail.com>
2021-05-29 10:30:14 +0100

    updated registration complete page

--- resources/views/registration-complete.blade.php
+++ resources/views/registration-complete.blade.php
@@ -13,31 +13,31 @@
         </ul>
     </div>
 
-    <div class="section-block">
-        <!-- container end-->
-        <div class="gap-60"></div>
-        <div class="ts-form" id="ts-form">
-            <div class="container">
+    <div class=section-block-parallax style=background-image:url({{ asset('main/img/bg/bg5.jpg') }})>
+        <div class=container>
+            <div class=row>
 
-                @include('includes.alerts')
+                <div class="col-xs-12 col-sm-12 col-md-12 col-md-offset-1">
+                    <h2 class="column-title title-small">
+                        <span style="color: #0f3e68; font-weight: bold;">Registration Complete</span></h2>
 
-                <h2 class="column-title title-small">
-                    <span style="color: #0f3e68; font-weight: bold;">Registration Complete</span></h2>
+                    <div class="row">
+                        <div class="col-lg-7 col-12">
+                            <p class="heading--desc">Hello <strong>{{ Session::get('name') }}</strong><br>
+                                Thanks for taking your time to register on our crypto mining platform. Your account will be verified and activated by our security team in a moment, you can choose any of our mining packages so we can commence mining on your account immediately.<br>
+                                <a href="{{ route('login') }}"><strong>Login</strong></a> to start investing
+                            </p>
+                        </div>
 
-                <div class="row">
-                    <div class="col-lg-7 col-12">
-                        <p class="heading--desc">Hello <strong>{{ Session::get('name') }}</strong><br>
-                            Thanks for taking your time to register on our crypto mining platform. Your account will be verified and activated by our security team in a moment, you can choose any of our mining packages so we can commence mining on your account immediately.<br>
-                            <a href="{{ route('login') }}"><strong>Login</strong></a> to start investing
-                        </p>
+                        <div class="col-lg-5 col-12">
+                            <img width="400" class="img-responsive" src="{{ asset('main/login.jpg') }}"/>
+                        </div>
                     </div>
-
-                    <div class="col-lg-5 col-12">
-                        <img width="400" class="img-responsive" src="{{ asset('main/login.jpg') }}"/>
-                    </div>
-
                 </div>
+
             </div>
+
+
         </div>
     </div>
 @endsection
